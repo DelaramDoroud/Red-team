fda84e6321fa0d5b548c3804eb80fd6173cfb8f2
unknown <delaramdoroud82@gmail.com>
2025-11-19 10:54:59 +0100

    chore(frontend): add ESLint, Prettier and Stylelint configuration

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['tests/**', 'vitest.config.js'] },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['#components', './assets/components'],
+          ['#js', './assets/js'],
+          ['#img', './assets/img'],
+          ['#modules', './assets/modules'],
+          ['#scss', './assets/scss'],
+          ['#config', './config'],
+        ],
+        extensions: ['.js', '.jsx', '.scss'],
+      },
+    },
+  },
+  env: { browser: true, node: true },
+  overrides: [
+    {
+      files: ['**/*.jsx', '**/*.js', '**/*.mjs'],
+      parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+    },
+    {
+      files: ['vitest.config.jsx'],
+      rules: { 'import/no-extraneous-dependencies': 'off' },
+    },
+    {
+      files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'],
+      env: {
+        jest: true,
+      },
+      globals: {
+        vi: 'readonly',
+      },
+    },
+  ],
+};
