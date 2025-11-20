80af27f0140aa0819831c3a934ad0ac7abb1db67
NaolZebene <zebenenaol@gmail.com>
2025-11-20 13:57:44 +0100

    feat/ created match settings model

---
+++ backend/models/match-setting.mjs
@@ -0,0 +1,53 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '#root/services/sequelize.mjs';
+
+const MatchSetting = sequelize.define(
+  'MatchSetting',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    problemTitle: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    problemDescription: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    referenceSolution: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    publicTests: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+    privateTests: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('draft', 'ready'),
+      allowNull: false,
+      defaultValue: 'draft',
+    },
+  },
+
+  {
+    tableName: 'match_setting',
+    schema: 'public',
+    timestamps: true,
+    indexes: [
+      {
+        name: 'match_setting_pkey',
+        unique: true,
+        fields: [{ name: 'id' }],
+      },
+    ],
+  }
+);
+
+export default MatchSetting;
