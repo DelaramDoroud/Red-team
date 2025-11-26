e296500d933d0e6695810042d6f01016ca3ab67f
Sepideh Mottaghi <sepideh.mottaghi@gmail.com>
2025-11-26 17:14:13 +0100

    fix: repair TailwindCSS integration

--- frontend/app/student/challenges/page.jsx
+++ frontend/app/student/challenges/page.jsx
@@ -64,7 +64,9 @@ export default function StudentChallengesPage() {
   return (
     <div className='max-w-4xl mx-auto p-6 space-y-6'>
       {/* <div className="w-96 h-32 bg-red-500 mb-100">TEST</div> */}
-      <h1 className='text-3xl font-bold text-white'>Available Challenges</h1>
+      <h1 className='text-3xl font-bold text-slate-900'>
+        Available Challenges
+      </h1>
 
       <div>
         {dummyData.map((c) => (
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
+    './app/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
     './assets/**/*.{js,jsx,ts,tsx,css,scss}',
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
+    // './pages/**/*.{js,jsx,ts,tsx}',
+    // './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
