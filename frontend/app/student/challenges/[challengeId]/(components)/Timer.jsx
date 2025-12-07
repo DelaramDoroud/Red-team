3747fd2dde419bd876a51dea1aa550768192f5ef
DelaramDoroud <delaramdoroud@gmail.com>
2025-12-07 01:01:11 +0100

    Implement match duration countdown

--- frontend/app/student/challenges/[challengeId]/(components)/Timer.jsx
+++ frontend/app/student/challenges/[challengeId]/(components)/Timer.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
 function Timer({ duration }) {
-  return <div>Timer:{duration}</div>;
+  const [timeLeft, setTimeLeft] = useState(null);
+
+  useEffect(() => {
+    // if (!duration) return;
+
+    const endTime = Date.now() + 120 * 1000;
+    
+    // setTimeLeft(duration);
+
+    const intervalId = setInterval(() => {
+      const diff = Math.floor((endTime - Date.now()) / 1000);
+
+      if (diff <= 0) {
+        setTimeLeft(0);
+        clearInterval(intervalId);
+      } else {
+        setTimeLeft(diff);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [120]);
+
+  function formatTime(seconds) {
+    if (seconds === null) return '--:--:--';
+    
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    const secs = String(seconds % 60).padStart(2, '0');
+    return `${hours}:${mins}:${secs}`;
+  }
+
+  return <div>Timer: {formatTime(timeLeft)}</div>;
 }
 
 export default Timer;
